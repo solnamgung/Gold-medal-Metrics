41343540fa2a8adb8848a968fdcb36341dc025b6
sol namgung <nandaras0103@gmail.com>
2022-04-20 12:12:01 +0900

    first commit

---
+++ src/main/java/com/project/goleMedal/GoleMedalApplication.java
@@ -0,0 +1,13 @@
+package com.project.goleMedal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoleMedalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoleMedalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/goleMedal/GoleMedalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.goleMedal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoleMedalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
